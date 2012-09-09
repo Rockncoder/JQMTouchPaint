3ff070cdcfd8a39c10a1a200cabcce7190a7b31f
Troy Miles <rockncoder@gmail.com>
2012-09-09 10:08:22 -0700

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,63 @@
+﻿
+
+var RocknCoder = RocknCoder || {};
+RocknCoder.Pages = RocknCoder.Pages || {};
+
+(function () {
+	"use strict";
+
+	// put the page events into one string
+	RocknCoder.PageEvents = "pagebeforeshow pageshow pagebeforechange pagechange pagebeforehide pagehide";
+
+	// the kernel remains unchanged
+	RocknCoder.Pages.Kernel = function (event) {
+		var that = this,
+			eventType = event.type,
+			pageName = $(this).attr("data-rnc-jspage");
+		if (RocknCoder && RocknCoder.Pages && pageName && RocknCoder.Pages[pageName] && RocknCoder.Pages[pageName][eventType]) {
+			RocknCoder.Pages[pageName][eventType].call(that);
+		}
+	};
+
+	// anonymous function which binds to the page's page events
+	(function () {
+		$("div[data-rnc-jspage]").on(
+			RocknCoder.PageEvents,
+			RocknCoder.Pages.Kernel
+		);
+	}());
+
+	// anonymous function which binds to the document's pageload event
+	(function () {
+		$(document).bind(
+			'pageload',
+			function (event, obj) {
+				console.log("pageload");
+				$("div[data-rnc-jspage]")
+					// to make sure we aren't double hooking events clear them all
+					.off(RocknCoder.PageEvents)
+					// then hook them all  (the newly loaded page is in DOM at this point)
+					.on(RocknCoder.PageEvents, RocknCoder.Pages.Kernel);
+			}
+		);
+	}());
+
+	// size the content area
+	RocknCoder.Dimensions = (function () {
+		var get = function () {
+			var isFirstPass = false,
+				isIPhone = (/iphone/gi).test(navigator.appVersion),
+				width = $(window).width(),
+				height = $(window).height() + (isIPhone ?  60 : 0),
+				hHeight = $('header').outerHeight(),
+				fHeight = $('footer').outerHeight();
+			return {
+				width: width,
+				height: height - hHeight - fHeight
+			};
+		};
+		return {
+			get: get
+		};
+	}());
+}());
---
+++ scripts/paintPage.js
@@ -0,0 +1,81 @@
+﻿
+
+var RocknCoder = RocknCoder || {};
+RocknCoder.Pages = RocknCoder.Pages || {};
+
+(function () {
+	"use strict";
+
+	RocknCoder.Pages.paintPage = (function () {
+		var ctx, lineStyle,
+			$window = $(window),
+			$canvas = $('#flexBox'),
+			canvas = $canvas.get(0),
+			// resize the canvas after retrieving the current dimensions
+			reSizeCanvas = function () {
+				var dims = RocknCoder.Dimensions.get();
+				$canvas.attr({
+					width: dims.width - 4,
+					height: dims.height - 4
+				});
+				return dims;
+			},
+			// get the context and create a yellow pen
+			setContext = function () {
+				ctx = canvas.getContext('2d');
+				lineStyle = {
+					strokeStyle: "rgba(250,250,0,1)",
+					lineWidth: 2,
+					lineCap: "round"
+				};
+				$.extend(ctx, lineStyle);
+			},
+			// do all of the canvas and context stuff
+			updateContext = function () {
+				window.scrollTo(1, 0);
+				reSizeCanvas();
+				setContext();
+			},
+			// extract the x&y
+			extractXY = function (oe) {
+				return {
+					x: oe.pageX,
+					y: oe.pageY
+				};
+			},
+			pageshow = function () {
+				// watch for orientation change or resize events
+				$window.bind('orientationchange resize', function (event) {
+					updateContext();
+					event.preventDefault();
+					return false;
+				});
+				// start tracking the touches, move the pen to the beginning of a line
+				$canvas.bind('touchstart', function (event) {
+					var xy = extractXY(event.originalEvent.touches[0]);
+					ctx.moveTo(xy.x, xy.y);
+					event.preventDefault();
+					return false;
+				});
+				// draw a line from the last point to this one
+				$canvas.bind('touchmove', function (event) {
+					var xy = extractXY(event.originalEvent.touches[0]);
+					ctx.lineTo(xy.x, xy.y);
+					ctx.stroke();
+					event.preventDefault();
+					return false;
+				});
+				updateContext();
+			},
+			// be a good citizen, unhook any events we hook
+			pagehide = function () {
+				$window.unbind('orientationchange resize');
+				$canvas.unbind('touchstart');
+				$canvas.unbind('touchmove');
+			};
+		return {
+			pageshow: pageshow,
+			pagehide: pagehide
+		};
+	}());
+}());
